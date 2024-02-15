071ed25a04cc780fbd2c4347a43b367a07815487
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2024-02-15 21:25:13 +0800

    Login pageFor Testing

---
+++ electives_threads/Controllers/UserLogin.cs
@@ -0,0 +1,14 @@
+﻿using electives_threads.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace electives_threads.Controllers
+{
+    public class UserLogin : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/UserLogin/Index.cshtml");
+        }
+    }
+}
