733d58d9297693943c9e6c7585b708ecfb050f81
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2024-02-15 21:25:13 +0800

    For Testing

--- electives_threads/DataConnection/DbContext.cs
+++ electives_threads/DataConnection/DbContext.cs
@@ -8,6 +8,8 @@ namespace electives_threads.DataConnection
 
     public class MySqlDbContext : DbContext
     {
+
+        //For Testing Pull
         public MySqlDbContext(DbContextOptions<MySqlDbContext> options) : base(options) { }
     }
 }
