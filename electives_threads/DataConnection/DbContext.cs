9fa7bc2b28efa5978d661846dda3f541286ee2e4
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2024-02-15 21:18:00 +0800

    Add project files.

---
+++ electives_threads/DataConnection/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+
+
+namespace electives_threads.DataConnection
+{
+
+
+    public class MySqlDbContext : DbContext
+    {
+        public MySqlDbContext(DbContextOptions<MySqlDbContext> options) : base(options) { }
+    }
+}
---
+++ electives_threads/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace electives_threads.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
